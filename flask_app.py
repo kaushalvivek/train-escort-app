f22ed35f8aeeddcc7f4f870ad3fb7f4a6ff48314
Vivek Kaushal <vivek.kaushal@research.iiit.ac.in>
2020-05-31 19:32:19 +0530

    added functionality to list multiple trains with same train number across days

--- flask_app.py
+++ flask_app.py
@@ -16,6 +16,56 @@ db = SQLAlchemy(app)
 app.config.from_object(__name__)
 Session(app)
 
+def prepare_output(output):
+  entries = []
+  # convert queries to objects
+  for obj in output:
+    entry = {
+      'train_no': obj.train_number,
+      'escort_name':obj.escort_name,
+      'escort_phone':obj.escort_phone,
+      'escort_from':obj.escort_from,
+      'escort_to':obj.escort_to,
+      'current':obj.current,
+      'datetime_created':obj.datetime_created,
+      'origin':obj.origin,
+      'terminating':obj.terminating,
+      'origin_date':obj.origin_date,
+      'updater_name':obj.updater_name,
+      'zone':obj.zone
+    }
+    # append query to objects
+    entries.append(entry)
+  
+  # get unique dates
+  origin_dates = {entry['origin_date'] for entry in entries}
+
+  # to store final output
+  final_output = []
+
+  # get latest entry for each unique date
+  for date in origin_dates:
+    all_with_date = []
+    for i in entries:
+      if (i['origin_date']==date):
+        all_with_date.append(i)
+    all_with_date.sort(key=lambda x:x['datetime_created'], reverse=True)
+    # add latest entry for each unique date to final_output
+    final_output.append(all_with_date[0])
+
+  # prepare final output for indian date time
+  final_output.sort(key=lambda x:x['origin_date'], reverse=True)
+  from_zone = tz.gettz('UTC')
+  to_zone = tz.gettz('Asia/Kolkata')
+  format = "%Y-%m-%d %H:%M:%S %Z%z"
+  naive_date = [i['datetime_created'].replace(tzinfo=from_zone) for i in final_output]
+  indian_datetime = [i.astimezone(to_zone).strftime(format) for i in naive_date]
+  for i in range(len(final_output)):
+    final_output[i]['datetime_created'] = indian_datetime[i]
+  return final_output
+
+
+
 class Escort(db.Model):
   transaction_id = db.Column(db.String, primary_key=True)
   datetime_created = db.Column(db.DateTime, default=datetime.now())
@@ -29,6 +79,7 @@ class Escort(db.Model):
   updater_name = db.Column(db.String)
   escort_from = db.Column(db.String)
   escort_to = db.Column(db.String)
+  origin_date = db.Column(db.String)
 
 def generate_random_string(stringLength=10):
   letters = string.ascii_lowercase
@@ -36,7 +87,7 @@ def generate_random_string(stringLength=10):
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+  return render_template('index.html', error=False)
 
 @app.route('/all')
 def all():
@@ -53,26 +104,27 @@ def all():
 @app.route('/data')
 def data():
   train_no = session.get('train_no')
-  output = Escort.query.filter(Escort.train_number==train_no).order_by(Escort.datetime_created).all()
-  from_zone = tz.gettz('UTC')
-  to_zone = tz.gettz('Asia/Kolkata')
-  format = "%Y-%m-%d %H:%M:%S %Z%z"
+  three_days_ago = datetime.now() - timedelta(4)
+  output = Escort.query.filter(Escort.train_number==train_no).filter(Escort.origin_date>=three_days_ago).order_by(Escort.datetime_created.desc()).all()
+  
+  # from_zone = tz.gettz('UTC')
+  # to_zone = tz.gettz('Asia/Kolkata')
+  # format = "%Y-%m-%d %H:%M:%S %Z%z"
+
+  if len(output) == 0:
+    return redirect('/error')
 
   try:
-    naive_date = output[-1].datetime_created.replace(tzinfo=from_zone)
-    indian_datetime = naive_date.astimezone(to_zone).strftime(format)
-    return render_template('data.html', latest=output[-1], all=output,indian_datetime=indian_datetime)
+    # naive_date = [i.datetime_created.replace(tzinfo=from_zone) for i in output]
+    # indian_datetime = [i.astimezone(to_zone).strftime(format) for i in naive_date]
+    final_output = prepare_output(output)
+    return render_template('data.html', output=final_output)
   except:
-    return render_template('error.html')
+    return redirect('/error')
 
 @app.route('/error')
 def error():
-  train_no = session.get('train_no')
-  output = Escort.query.filter(Escort.train_number==train_no).order_by(Escort.datetime_created).all()
-  try:
-    return render_template('data.html', latest=output[-1], all=output)
-  except:
-    return render_template('error.html')
+    return render_template('index.html', error=True)
 
 
 @app.route('/get_train_no')
@@ -85,6 +137,7 @@ def get_train_no():
 def get_data():
   train_no = int(request.args.get('train_no'))
   origin = request.args.get('origin')
+  origin_date = request.args.get('origin_date')
   terminating = request.args.get('terminating')
   current = request.args.get('current')
   escort_phone = request.args.get('escort_phone')
@@ -96,7 +149,7 @@ def get_data():
   eid = generate_random_string(15)
   date = datetime.now()
   new_escort = Escort(transaction_id=eid, datetime_created=date,\
-  train_number=train_no,origin=origin,terminating=terminating,\
+  train_number=train_no,origin=origin,origin_date=origin_date,terminating=terminating,\
   current=current,escort_name=escort_name,escort_phone=escort_phone,\
   zone=zone, updater_name=name, escort_from=escort_from, escort_to=escort_to)
   db.session.add(new_escort)
